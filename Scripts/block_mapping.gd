75e6b1abdb95362156770dd41e7667d0c42af9e4
FieryBlaze <fieryblaze6799@gmail.com>
2015-05-16 22:30:16 -0700

    Map and Indestructible block scenes added, with basic testing funtionality

---
+++ Scripts/block_mapping.gd
@@ -0,0 +1,31 @@
+
+extends Node2D
+
+var map = [[]]
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+var IndestructibleBlock = preload("res://Scenes/Blocks/IndestructibleBlock.scn")
+
+var sizeX = 77
+var sizeY = 27
+
+func _ready():
+	while(map.size() < 10):
+		map.append([])
+	for i in range(10):
+		while(map[i].size() < 10):
+			map[i].append(IndestructibleBlock.instance())
+	
+	for x in range(10):
+		for y in range (10):
+			self.add_child(map[x][y])
+			map[x][y].set_pos(Vector2((x * sizeX) + (sizeX / 2),(y * sizeY) + (sizeY/2)))
+	print(map.size())
+	print(map[0].size())
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	pass
+
